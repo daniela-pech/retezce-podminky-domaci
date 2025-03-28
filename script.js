52e1f004236ce93dfcf8118821a3931e3bd2d797
Daniela Pech <randakova.daniela@gmail.com>
2025-03-28 16:15:13 +0100

    vypracovane cviceni Množstevní ceny

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //FIT e-mail - Vytvořte e-mailovou adresu pro studenty FIT ČVUT.
-
+/*
 const jmeno = {
   krestni: prompt('Zadej křesní jméno'),
   prijmeni: prompt('Zadej prijmeni'),
@@ -12,4 +12,26 @@ document.body.innerHTML += `
 <p>
 ${finalPrijmeni}${finalKrestni}@${domena}
 </p>
+`;
+*/
+// Množstevní slevy - Napište program, který spočítá cenu objednávky triček s potiskem.
+
+const pocetKusu = prompt('Zadej počet kusů objednávky.');
+let cenaKus = '';
+if (pocetKusu < 50) {
+  cenaKus = 300;
+} else if (pocetKusu < 200) {
+  cenaKus = 250;
+} else if (pocetKusu < 500) {
+  cenaKus = 200;
+} else if (pocetKusu < 1000) {
+  cenaKus = 150;
+} else {
+  cenaKus = 120;
+}
+const cenaCelkem = cenaKus * pocetKusu;
+document.body.innerHTML += `
+<p>Odběr kusů: ${pocetKusu}</p>
+<p>Cena za kus: ${cenaKus} Kč</p>
+<h2>Celková cena: ${cenaCelkem} Kč</h2>
 `;
